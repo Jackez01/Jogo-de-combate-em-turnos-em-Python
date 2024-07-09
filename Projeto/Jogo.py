f978ebc93db4d72be75885e63f5456d2a258cd97
Lucas Geraldi Freitas <lucasgefreitas@gmail.com>
2024-07-09 14:15:23 -0300

    Herói com ataque especial e inimigo atacando

--- Projeto/Jogo.py
+++ Projeto/Jogo.py
@@ -41,6 +41,11 @@ class Heroi(Personagem):
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
 
+    def ataque_especial(self, alvo):
+        dano = self.get_nivel() * 5 #Dano x5
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
+
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo) -> None:
         super().__init__(nome, vida, nivel)
@@ -72,8 +77,15 @@ class Jogo:
 
             if escolha == '1':
                 self.heroi.atacar(self.inimigo)
+
+            elif escolha == '2':
+                self.heroi.ataque_especial(self.inimigo)
+
             else:
                 print("Escolha inválida. Escolha novamente")
+
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.atacar(self.heroi)
 
         if self.heroi.get_vida() > 0:
             print("\nParabéns, você ganhou!")
